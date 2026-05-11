54c718c0f5140d7b4c606d9864be2ebfc3f608cc
Nishant Kumar <nishantaipath@gmail.com>
2026-05-12 00:39:48 +0530

    Latest push

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ export const ProtectedRoute = ({ children, adminOnly = false }) => {
   if (loading) return null; // Wait for Firebase to check the user
 
   // 1. If not logged in at all, go to Login
-  if (!user) return <Navigate to="/login" replace />;
+  if (!user) return <Navigate to="/" replace />;
 
   // 2. If trying to enter Mentor side, check the Email
   const isMentor = user.email === 'nishant53195@gmail.com'; 
